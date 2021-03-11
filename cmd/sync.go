fb61a49dc1452231f80c1d24bcc00155a883339a
Alec Benzer <alecbenzer@gmail.com>
2021-03-11 15:25:49 -0500

    refresh -> sync

--- cmd/sync.go
+++ cmd/sync.go
@@ -4,9 +4,8 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// refreshCmd represents the refresh command
-var refreshCmd = &cobra.Command{
-	Use:   "refresh",
+var syncCmd = &cobra.Command{
+	Use:   "sync",
 	Short: "Bring the branch up-to-date with the main branch",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -22,15 +21,5 @@ to quickly create a Cobra application.`,
 }
 
 func init() {
-	rootCmd.AddCommand(refreshCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// refreshCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// refreshCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.AddCommand(syncCmd)
 }
