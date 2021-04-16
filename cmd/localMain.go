b2fe638099b52472a2780715b7275e23f01fab30
Alec Benzer <alecbenzer@gmail.com>
2021-04-16 17:35:13 -0400

    local-main command

---
+++ cmd/localMain.go
@@ -0,0 +1,34 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/go-git/go-git/v5"
+	"github.com/spf13/cobra"
+)
+
+// localMainCmd represents the localMain command
+var localMainCmd = &cobra.Command{
+	Use:   "local-main",
+	Short: "Check out a local main branch",
+	Run: func(cmd *cobra.Command, args []string) {
+		fetch("origin", config.MainBranch)
+
+		r, err := git.PlainOpen(".")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = r.DeleteBranch(config.MainBranch)
+		if err != nil && err != git.ErrBranchNotFound {
+			log.Fatal(err)
+		}
+
+		runGit("switch", "--create", config.MainBranch, fmt.Sprintf("origin/%s", config.MainBranch))
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(localMainCmd)
+}
